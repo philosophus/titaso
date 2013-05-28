78d18e840bbcc832c58a4c4c725054227dd10db8
Florian Dahms <dahms@or.rwth-aachen.de>
2013-05-28 14:15:20 +0200

    Adding a class for problem instances.

---
+++ src/main/java/de/rwthaachen/hyperhallsolver/Instance.java
@@ -0,0 +1,17 @@
+package de.rwthaachen.hyperhallsolver;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Florian Dahms <dahms@or.rwth-aachen.de>
+ */
+public class Instance {
+
+    Map<String, Object> rawInstanceData;
+
+    public Instance(Map<String, Object> rawInstanceData) {
+        this.rawInstanceData = rawInstanceData;
+    }
+
+}
